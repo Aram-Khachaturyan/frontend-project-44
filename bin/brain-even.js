b4dfa485870c5726d78fd44a87ab83d655b5b417
Aram Khachaturyan <aram@StasPc.>
2025-01-04 21:01:34 +0300

    add brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 function getRandom() {
-  const num = Math.random() * 100;
-  if (num % 2 === 0) {
-    console.log('yes')
+  
+
+  for (let i = 0; i < 3; i=+1) {
+  const randomNum = Math.round(Math.random() * 100); {
+  console.log('Your number is ${randomNum}');}
+
+  const gamerAnswer = readlineSync.question(' ');
+  const rightAnswer = (randomNum % 2 === 0);
+  if (gamerAnswer === rightAnswer) {
+    console.log('Correct!')
   }
+    
   else {
-  console.log('no')}
+  console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, ${name}!')};
+  return;
 };
-getRandom();
+}
+console.log('Congratulations, ${name}!');
