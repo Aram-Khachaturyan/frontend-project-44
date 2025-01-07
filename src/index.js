29b0f1c2b0dd494eaa548e92a511d1a349f6449d
Aram Khachaturyan <aram@StasPc.>
2025-01-07 22:35:43 +0300

    prepare for first project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import readlineSync from 'readline-sync';
 import greetUser from './welcome.js';
-const runGame = () => { greetUser (); }
 
-const randomNum = Math.round(Math.random() * 100); 
-  console.log(`Your number is ${randomNum}`);
-  
+const runGame = () => {
+  // создаём функцию игры
+
+greetUser();
+  // импорттруем приветствие
+
+  // импортируем дексрипшн игры
+  // создаём логику игры
+
+  // консоль для вывода вопроса
+    console.log(`Your number is ${randomNum}`);
+
+  // ввод ответа и проверка ответа игрока
   const gamerAnswer = readlineSync.question('Your answer: ');
   if (gamerAnswer === rightAnswer) {
-  console.log('Correct!')
-  }
 
-  else {
-  console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, ${name}!')}
-  
+   // консоль при правильном ответе
+    console.log('Correct!');
 
+    // тут должен быть цикл и консоль с поздравлением
 
+}
+// проверка и консоль для неправильных ответов
+else {
+
+console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, ${name}!')};
+};
+
+// экспортируем функцию с игрой
 export default runGame;
-
