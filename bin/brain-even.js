35342c1cc3296f0d605678332a29cccc6c8034f0
Aram Khachaturyan <aram@StasPc.>
2025-01-07 20:26:58 +0300

    Preparing for the first project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-const isEven = () => (randomNum % 2 === 0 ? "yes" : "no" );
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomNum = Math.round(Math.random() * 100); 
-  console.log(`Your number is ${randomNum}`);
-  
-  const gamerAnswer = readlineSync.question('Your answer: ');
-  const rightAnswer = (randomNum % 2 === 0 ? "yes" : "no" );
-  if (gamerAnswer === rightAnswer) {
-  console.log('Correct!')
-  }
+import runGameEven from "../src/games/even.js";
 
-  else {
-  console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, ${name}!')}
-
-  const runGame = () => {
-    
-  }
-  
-  export default runGame;
+runGameEven();
---
+++ src/games/even.js
@@ -0,0 +1,16 @@
+import { getRandomNumber } from '../helper.js';
+import { runGame } from '../index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (randomNumber) => randomNumber % 2 === 0;
+
+const getRound = () => {
+
+};
+
+const runGameEven = () => {
+
+};
+
+export default runGameEven;
