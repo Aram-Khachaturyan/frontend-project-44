6694a3c322534ca410f19f88e585e093b90c064b
Aram-Khachaturyan <aram.vl@list.ru>
2025-01-28 23:16:27 +0300

    add 4th game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import runGameGcd from '../src/games/gcd.js';
+import runGameProgression from '../src/games/progression.js';
 
-runGameGcd();
+runGameProgression();
--- src/games/progression.js
+++ src/games/progression.js
@@ -3,14 +3,34 @@ import getRandomIntInclusive from '../general.js';
 
 const description = 'What number is missing in the progression?';
 
-const generateProgression = (start, length, step) = {
+//создаем переменную с прогрессией.отдаём ей в функции старт длину и шаг.создаём цикл, который будет 
+// рандомно закидывать пустое место
+
+const generateProgression = (start, length, step) => {
     const progression = [];
-    for (let i = 0; i < length; i += 1);
-    }
+    for (let i = 0; i < length; i += 1) {
+    const stepFor = progression.push(start + step * i); }
+console.log(stepFor);
+    return progression;
 
-}
+};
+    // начинаем Раунд
+    // определяем переменные старт, шаг, длину с использованием рандома
+    // переменная вопрос в котору входят все указанные переменные
+    // переменная ответ для определения правильности
+    // возврат вопроса ответ
 
-const start = getRandomIntInclusive ();
-const randomLength = getRandomIntInclusive (5, 10);
-const step = getRandomIntInclusive (1, 10);
+const getRound = () => {
+  const start = getRandomIntInclusive(1, 10);
+  const length = getRandomIntInclusive(5, 10);
+  const step = getRandomIntInclusive(1, 10);
+  const question = [start, length, step];
+  const answer = (progression === question);
+  return [question, answer];
+};
 
+const runGameProgression = () => {
+    runGame (description, getRound);
+};
+
+export default runGameProgression;
